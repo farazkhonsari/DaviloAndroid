2f537ffc4e32c529ca2ee5155f339ceaf82a4bc6
faraz khonsari <FarazKhonsari@gmail.com>
2020-08-27 09:50:08 +0430

    Faraz: ,

--- app/src/main/java/org/davilo/app/main/MainActivity.kt
+++ app/src/main/java/org/davilo/app/main/MainActivity.kt
@@ -65,6 +65,7 @@ class MainActivity : AppCompatActivity() {
            setupActionBarWithNavController(navController) })
 
         currentNavController = controller
+
     }
 
     override fun onSupportNavigateUp(): Boolean {
